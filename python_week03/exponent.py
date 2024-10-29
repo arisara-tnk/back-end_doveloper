8906cf2305579c09d78747cf538072fcd932ca2e
arisara_tnk <arisarasomsak08@gmail.com>
2024-10-29 12:29:18 +0700

    สูตรคูณ

--- python_week03/exponent.py
+++ python_week03/exponent.py
@@ -1,3 +1,3 @@
 num = int(input('เลขยกกำลัง'))
-for i in range(1,10,1):
+for i in range(1,num+1,1):
     print(f'{i} ยกกำลัง {i} = {i**2}')
---
+++ python_week03/multiply.py
@@ -0,0 +1,3 @@
+num = int(input('แม่สูตรคูณ'))
+for i in range(1,25,1):
+    print(f'{num} x {i} = {num*i}')
